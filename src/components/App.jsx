e4ca9bde612f7b372723920ea6ea3ff160bd0640
OKirsenko <senka3434@gmail.com>
2022-02-09 11:44:39 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,6 +13,18 @@ export default class App extends Component {
     ],
     filter: '',
   };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
   formSubmit = contact => {
     if (
       this.state.contacts.find(item =>
